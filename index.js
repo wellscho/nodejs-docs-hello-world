261d024979e05a68e32de4c813bb2a9ff253a4d9
wellscho <wellscho@hotmail.com>
2019-09-29 11:52:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World Wells 你好!");
+    response.end("Hello World Wells 1337!");
 });
 
 const port = process.env.PORT || 1337;
